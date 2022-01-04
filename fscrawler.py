839534bbcb29003274f7b15f3edd7c1a3aa6a56c
Jason Murphy <jason.murphy100@gmail.com>
2022-01-04 21:09:26 +0000

    Outputs folder contents

--- fscrawler.py
+++ fscrawler.py
@@ -3,23 +3,38 @@ import json
 
 
 dir = "exampleDir"
+# tree2 = {}
+# tree2["exampleDir/foobar"] = ["exec", "DS_store"]
+tree = {}
+# tree[dir] = [tree2]
 
-def crawl_dir(dir):
-    dir_contents = os.listdir(dir)      # get files and directories in the given directory
+
+# result = json.dumps(tree, indent = 2)
+
+
+
+
+def crawl_dir(dir, prev_tree):
+    dir_contents = os.listdir(dir)        # get list of files and directories in the given directory
+    current_tree = {}
+    folder = []
+    prev_tree[dir] = folder
 
     for path in dir_contents:
         path = os.path.join(dir, path)    # create full file path
-        print(path)
         if os.path.isdir(path):
-            crawl_dir(path)
+
+            crawl_dir(path, prev_tree)
+    
+        folder.append(path)
+    return prev_tree
+    
 
 
-crawl_dir(dir)
+output = crawl_dir(dir, tree)
+json_tree = json.dumps(output, indent=2)
+print(json_tree)
 
-
-
-# result = glob(os.path.join(dir, "*", ""))
-# print(result)
 
 # json_data = json.dumps(dir_tree)
 #print(json_data)
