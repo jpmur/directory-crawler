05cdedd45094a8c0817b0f7b0f9a65a46c3b47cb
Jason Murphy <jason.murphy100@gmail.com>
2022-01-05 21:17:00 +0000

    Prints JSON tree

--- fscrawler.py
+++ fscrawler.py
@@ -2,42 +2,27 @@ import os
 import json
 
 
-dir = "exampleDir"
-# tree2 = {}
-# tree2["exampleDir/foobar"] = ["exec", "DS_store"]
-tree = {}
-# tree[dir] = [tree2]
-
-
-# result = json.dumps(tree, indent = 2)
-
-
-
-
-def crawl_dir(dir, prev_tree):
-    dir_contents = os.listdir(dir)        # get list of files and directories in the given directory
+def crawl_dir(dir, parent_tree, parent_dir):
+    dir_contents = os.listdir(dir)        
     current_tree = {}
     folder = []
-    prev_tree[dir] = folder
+    current_tree[dir] = folder                          # create new key/value pair (JSON object) for this directory
+    if dir != parent_dir:                               # will only be true for first iteration (top level dir)
+        parent_tree[parent_dir].append(current_tree)    # insert new JSON object into the parent dir's value array
 
     for path in dir_contents:
-        path = os.path.join(dir, path)    # create full file path
+        path = os.path.join(dir, path)    
         if os.path.isdir(path):
-
-            crawl_dir(path, prev_tree)
+            crawl_dir(path, current_tree, dir)
+        else:
+            folder.append(path)
+    return current_tree
     
-        folder.append(path)
-    return prev_tree
-    
 
 
-output = crawl_dir(dir, tree)
+dir = "exampleDir"
+tree = {}
+tree[dir] = []
+output = crawl_dir(dir, tree, dir)
 json_tree = json.dumps(output, indent=2)
 print(json_tree)
-
-
-# json_data = json.dumps(dir_tree)
-#print(json_data)
-
-# result = os.listdir(path)
-# print(result)
